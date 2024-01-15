7e2cb6676df1a34d875a064fa6c9d3b3b3aabc28
md_irfaan <mohamedirfaan71@gmail.com>
2024-01-15 17:46:08 +0530

    Change 5

---
+++ CURA_Health_Care/Test Suites/001_CURA_TEST_SUITE_LOGIN_AND_NAV_LINKS_VALIDATION.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>001_CURA_TEST_SUITE_LOGIN_AND_NAV_LINKS_VALIDATION</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>650af8d4-4f13-44e5-926b-e8b8056c6dbf</testSuiteGuid>
+   <testCaseLink>
+      <guid>3c241ea3-b74e-411e-bd3c-6f723953eb8b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/001_CURA_Login_Validation</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4309fb37-30fe-4eea-bad7-c1fcfa71cc79</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/002_CURA_Navbar_Links_Validation</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
