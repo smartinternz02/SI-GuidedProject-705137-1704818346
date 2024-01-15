c1b8062a97c78d0ed754b60ea34296aba9207e20
md_irfaan <mohamedirfaan71@gmail.com>
2024-01-15 18:10:14 +0530

    Test_Suite_Push

---
+++ CURA_Health_Care/Test Suites/002_CURA_TEST_SUITE_MAKE_AN_APPOINTMENT.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>002_CURA_TEST_SUITE_MAKE_AN_APPOINTMENT</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>7f3e317f-c5de-4f4d-af85-02a935372294</testSuiteGuid>
+   <testCaseLink>
+      <guid>fc84b626-6302-4dac-a39d-03a7304913d8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/001_CURA_Login_Validation</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b4d176c0-3bc7-465d-a3af-7d50481bc8dc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/003_CURA_Validate_for_making_appointment</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
