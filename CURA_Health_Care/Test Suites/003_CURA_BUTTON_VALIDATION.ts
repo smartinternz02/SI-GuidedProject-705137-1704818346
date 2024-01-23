b1f0b71ac5d5585ed42757188eb195f008001c64
md_irfaan <mohamedirfaan71@gmail.com>
2024-01-23 21:28:29 +0530

    xb

---
+++ CURA_Health_Care/Test Suites/003_CURA_BUTTON_VALIDATION.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>003_CURA_BUTTON_VALIDATION</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>94cb227b-a2ce-41ed-8a23-c09d04bbedf6</testSuiteGuid>
+   <testCaseLink>
+      <guid>9d568e28-f9dd-467d-9812-6a08e4da4cb1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/001_CURA_Login_Validation</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>228e7656-06a8-405c-b4be-c70ad8c5354b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/004_CURA_Validate_Drop_Down_Button</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
